64af4d48b13b4260c3e25fb3d59d9c7926f899ab
sirinapaphaderm <sirinapa.phad57@sskru.ac.th>
2016-09-09 14:26:33 +0700

    new project

---
+++ Hello_OpenSource/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_OpenSource
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello My Name is Sirinapa Phaderm 5711423124!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
